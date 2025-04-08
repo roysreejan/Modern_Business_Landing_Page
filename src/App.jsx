c789aff1c7a0e41194924acc7c9e3cd3a1bdf21c
roysreejan <sreejanroy01@gmail.com>
2025-04-08 21:25:49 +0600

    About us section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import CompanyLogo from "./components/CompanyLogo";
+import FeaturesSection from "./components/FeaturesSection";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import PurposeSection from "./components/PurposeSection";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Navbar />
         <Hero />
         <CompanyLogo />
+        <PurposeSection />
+        <FeaturesSection />
       </div>
     </main>
   );
