16373e3df2e5053e630c8aca11b097187da0b3e6
roysreejan <sreejanroy01@gmail.com>
2025-04-08 19:02:08 +0600

    Company logo and animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import CompanyLogo from "./components/CompanyLogo";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -9,6 +10,7 @@ function App() {
       <div className="overflow-hidden">
         <Navbar />
         <Hero />
+        <CompanyLogo />
       </div>
     </main>
   );
